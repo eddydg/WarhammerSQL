57bf2196749ad8dc59527d78d891fe5dcdd1a48c
eddydg <diablo22007@gmail.com>
2016-04-12 15:43:10 +0200

    Added Exo 8

---
+++ Exo8.sql
@@ -0,0 +1,30 @@
+﻿   WITH units_by_category AS (
+    SELECT
+        units.id,
+        units.name,
+        squads.category,
+        points,
+        extract(year from upkeeps.scheduled) as year
+    FROM squads
+    JOIN units ON units.squad_id = squads.id
+    JOIN upkeeps on upkeeps.unit_id = units.id
+), results AS (
+    SELECT
+    distinct on (category, year)
+	category,
+        sum(points) OVER (PARTITION BY category, year) AS total,
+        year
+    FROM units_by_category as u
+)
+SELECT
+	curr_year,
+    (SELECT total FROM results WHERE category = 'HQ' and year = curr_year) as "	HQ",
+    (SELECT total FROM results WHERE category = 'Elites' and year = curr_year) as "Elites",
+    (SELECT total FROM results WHERE category = 'Troops' and year = curr_year) as "Troops",
+    (SELECT total FROM results WHERE category = 'Fast Attack' and year = curr_year) as "Fast Attack",
+    (SELECT total FROM results WHERE category = 'Heavy Support' and year = curr_year) as "Heavy Support"
+FROM generate_series(
+		(SELECT min(extract(year FROM scheduled)::INT) FROM upkeeps),
+		(SELECT max(extract(year FROM scheduled)::INT) FROM upkeeps)
+	) as curr_year
+;
