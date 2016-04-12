293cb4732db9e6f6ee5acd962b2d3c5b9077fa33
eddydg <diablo22007@gmail.com>
2016-04-12 17:50:47 +0200

    Basis for Exo9

---
+++ Exo9.sql
@@ -0,0 +1,29 @@
+﻿/*
+foreach squad_chief (where leader_id = null)
+	foreach units (where units.squad_id = squad_chief.id)
+	foreach squad (where squad.chef_id = squad_chief.id)
+		foreach units (where units.squad_id = squad.id)
+
+*/
+
+WITH leaders AS (
+	select
+		squads.id as squad_id,
+		concat('(', squads.category, ') ', squads.name, ' [', squads.id, E']\n') as title
+	from squads
+	where leader_id IS NULL
+)
+select
+	concat(
+		leaders.title,
+		(SELECT string_agg(concat(E'\t', units.name, ' [', units.id, ']'), E'\n') FROM units WHERE units.squad_id = leaders.squad_id),
+		E'\n',
+		(SELECT string_agg(concat(E'\t(', squads.category, ') ', squads.name, ' [', squads.id, ']'), E'\n')
+			FROM squads
+			WHERE squads.leader_id = leaders.squad_id)
+	)
+FROM leaders;
+;
+
+
+
